47688f005680248a2ec47801e9d42bf98f49b96c
Ubpa <641614112@qq.com>
2020-02-28 11:10:00 +0800

    add static lib

--- include/ANN/ANN.h
+++ include/ANN/ANN.h
@@ -59,7 +59,7 @@
 #ifndef ANN_H
 #define ANN_H
 
-#ifdef WIN32
+#if defined(WIN32) && defined(USE_ANN_DLL)
   //----------------------------------------------------------------------
   // For Microsoft Visual C++, externally accessible symbols must be
   // explicitly indicated with DLL_API, which is somewhat like "extern."
